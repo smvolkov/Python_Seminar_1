076337c4db888fca89f5daf9ec7a61fbcf074949
Semion Volkov <semen_volkov@mail.ru>
2023-01-10 14:38:25 +0500

    Task2 added

---
+++ Task2.py
@@ -0,0 +1,13 @@
+# Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+
+# x = bool(input('Введите x: '))
+# y = bool(input('Введите y: '))
+# z = bool(input('Введите z: '))
+
+for x in range(0, 2):
+    for y in range(0, 2):
+        for z in range(0, 2):
+            if not (x or y or z) == (not(x) or not(y) or not(z)):
+                print(f'For x = {x}, y = {y}, z = {z}: TRUE')
+            else:
+                print(f'For x = {x}, y = {y}, z = {z}: FALSE')
